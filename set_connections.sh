d0805bd0a6629416764e9554608aef42cb0c254f
eli <ellizzabbetth@gmail.com>
2024-02-11 12:32:33 -0500

    first commit

---
+++ set_connections.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+source .env
+echo $AIRFLOW_UID
+echo $AWS_KEY
+echo $AWS_SECRET
+
+printValue()
+{
+  section="$1"
+  param="$2"
+  found=false
+  while read line
+  do
+    [[ $found == false && "$line" != "[$section]" ]] &&  continue
+    [[ $found == true && "${line:0:1}" = '[' ]] && break
+    found=true
+    [[ "${line%=*}" == "$param" ]] && { echo "${line#*=}"; break; }
+  done
+}
+
+dwh_host=$(printValue CLUSTER dwh_host < /etc/applications.conf)
+echo $dwh_host
+echo $DWH_DB_PASSWORD
+echo $DWH_DB_USER
+
+# ./airflow info
+# docker airflow-cli info
+
+
+# docker exec -it cd12380-data-pipelines-with-airflow-airflow-cli bin/bash
+./airflow.sh airflow users create --email student@example.com --firstname aStudent --lastname aStudent --password admin --role Admin --username admin
+
+./airflow.sh airflow connections add 'redshift' --conn-type 'postgres' --conn-login $DWH_DB_USER --conn-password $DWH_DB_PASSWORD --conn-host $dwh_host --conn-port 5439 --conn-schema 'dev'
+./airflow.sh airflow connections add 'aws_credentials' --conn-type 'aws' --conn-login $AWS_KEY --conn-password $AWS_SECRET
+
+
+./airflow.sh airflow connections add 'redshift' --conn-type 'postgres' --conn-login 'awsuser' --conn-password 'R3dsh1ft' --conn-host "default-workgroup.c460mqtebvyp.us-east-1.redshift.amazonaws.com" --conn-port 5439 --conn-schema 'dev'
+./airflow.sh airflow connections add 'aws_credentials' --conn-type 'aws' --conn-login 'AKIAYM24S7U7LJWGSNNK' --conn-password '1oDEChbmnnmD4C/Vz6dzmLgRDhvtQZt3pzg3lL2U'
+
+#https://unix.stackexchange.com/questions/444296/section-wise-variable-accessing-from-config-file
